54ce80a31fc78614e503aab5d20db100317eee78
mikkoki <mikkoki@Ubuntu.myguest.virtualbox.org>
2023-05-23 09:49:27 +0300

    ex5

---
+++ Esiopinnot/Viikko3/ex5/ex5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cctype>
+
+/*int getLastNumber(std::string number) {
+
+    for (int i = number.length() - 1; i >= 0; i--) {
+        
+        if (isdigit(number[i])) {
+            return number[i];
+        }
+    }
+
+    return 0;
+
+}*/
+
+int getLastNumber(std::string str) {
+    int lastNumber = 0;
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        if (isdigit(str[i])) {
+
+            lastNumber = str[i] - '0';
+            
+            return lastNumber;
+        }
+    }
+
+    return lastNumber;
+}
+
+int main()
+{
+    std::string number;
+
+    std::cout << "Enter person number: ";
+    std::cin >> number;
+
+    if (getLastNumber(number) % 2 == 0) {
+        std::cout<<"You are a woman.";
+    }
+    else{
+        std::cout<<"You are a man.";
+    }
+
+    return 0;
+}
