8895f5d48554a17a8da288d318a6f44d5c12985c
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-06-06 10:22:28 +0300

    Add comment for function
    -Adding comment for the reverseString function

--- week1/Tuesday/assignment1/main.cpp
+++ week1/Tuesday/assignment1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-
+//Takes a string as input and returns the reverse of that string.
 std::string reverseString(std::string string){
     std::string reversedStr;
     for (int i = string.length() - 1; i >= 0; --i) {
